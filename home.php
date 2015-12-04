7db0cf7cacfbb731fb4034bf40f4d44bee96bb15
chen-ting <tsing_cih@126.com>
2015-12-04 10:57:26 +0800

    a test

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+    define('THINK_PATH', './ThinkPHP/');
+    define('APP_PATH','./home/');
+    define('APP_NAME','home');
+    define("APP_DEBUG", true);
+    require  THINK_PATH.'./ThinkPHP.php';
+
+
+?>
