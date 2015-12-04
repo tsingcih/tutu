64f16a83e77e66f9bee60af10f20727ca6b49285
chen-ting <tsing_cih@126.com>
2015-12-04 17:34:17 +0800

    2015-12-4  bm

--- admin/Lib/Action/IndexAction.class.php
+++ admin/Lib/Action/IndexAction.class.php
@@ -12,13 +12,12 @@ class IndexAction extends Action {
         $Data=M('Data');
         $data['name']='ThinkPHP';
         $data['id']=array('exp','id+1');
-        $Data->where('data=thinkphp')->save($data);
-        
-        
-        $this->display();
-	    /*$Data=M('Data');//实例化Data数据模型
+        $Data->where('data=thinkphp')->save($data);   
+         
+       
+	    $Data=M('Data');//实例化Data数据模型
 	    $this->data=$Data->select();        
-        $this->display();   */
+        $this->display();   
         #$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
     public function Form(){
--- admin/Runtime/Cache/4c18d5952d0dbf1eedb8a1ec796fa788.php
+++ admin/Runtime/Cache/4c18d5952d0dbf1eedb8a1ec796fa788.php
@@ -1,10 +1,44 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>admin</title>
+	<meta charset="UTF-8">
+	<title>admin</title>
+    <script>  
+          
+            /*  
+                计算两个日期的时间间隔天数  
+            */  
+            //时间字符串的格式：月-日-年  
+            var date1 = "12-3-2013";  
+            var date2 = "6-6-2013";  
+          
+            //讲时间字符串转化为距离1970年1月1日午夜零时的时间间隔的毫秒数  
+            var time1 = Date.parse(date1);  
+            var time2 = Date.parse(date2);  
+              
+            //讲两个时间相减，求出相隔的天数  
+            var dayCount = (Math.abs(time1 - time2))/1000/60/60/24;  
+              
+            alert(dayCount);  
+        </script>
 </head>
 <body>
+	<div>
 	<?php if(is_array($data)): $i = 0; $__LIST__ = $data;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i; echo ($vo["id"]); ?>--<?php echo ($vo["data"]); ?></br><?php endforeach; endif; else: echo "" ;endif; ?>
+	</div>
+	
+	<div>
+	<li>
+		<a href="__APP__/Form/Form.html">登录</a>	
+	</li>
+	</div>
+	
+	<div id="time"></div>  
+  
+  
+
+	
+
 </body>
 </html>
